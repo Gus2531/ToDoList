79e46628a52860e376ec6ab42741403dac1c1d2f
0-GK-0 <guguscaff7@gmail.com>
2026-09-10 15:34:52 -0300

    pronto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+import {useState} from 'react';
+
+function App() {
+    const [tarefa, setTarefa] = useState([]);
+
+
+    return(<>
+        <div>
+            <h1>my to-do-list</h1>
+        </div>
+
+    </>);
+}
+
+export default App();
